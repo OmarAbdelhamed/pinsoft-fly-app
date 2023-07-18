9d8c9dd5b6f4304141544d3928d8934c6f24f5b2
burakmetinn <burakmetin579@gmail.com>
2023-07-18 13:37:33 +0300

    .

--- src/screens/FlyingDetailScreen.js
+++ src/screens/FlyingDetailScreen.js
@@ -3,14 +3,18 @@ import PriceDetail from "../components/FlyingDetailScreen/PriceDetail";
 import PassengerDetail from "../components/FlyingDetailScreen/PassengerDetail";
 import ContactDetail from "../components/FlyingDetailScreen/ContactDetail";
 import BillDetail from "../components/FlyingDetailScreen/BillDetail";
+import FlyInformation from "../components/FlyingDetailScreen/FlyInformation";
+import FlyTicketInsurance from "../components/FlyingDetailScreen/FlyTicketInsurance";
 
 const FlyingDetailScreen = () => {
   return (
     <div>
+      <FlyInformation />
       <PriceDetail />
       <PassengerDetail />
       <ContactDetail />
       <BillDetail />
+      <FlyTicketInsurance />
     </div>
   );
 };
