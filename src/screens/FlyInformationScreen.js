99f8830b65d3896059261292028cde16819c4b66
Melihcan Şimşek <melihsimsek02@gmail.com>
2023-07-18 15:37:53 +0300

    changes

--- src/screens/FlyInformationScreen.js
+++ src/screens/FlyInformationScreen.js
@@ -4,20 +4,24 @@ import PassengerInformation from '../components/FlyInformationScreen/PassengerIn
 import MainHeader from '../components/FlyInformationScreen/MainHeader'
 import MiniHeader from '../components/FlyInformationScreen/MiniHeader'
 import InsuranceInformation from '../components/FlyInformationScreen/InsuranceInformation'
+import Header from './Header'
 
 function FlyInformationScreen() {
   return (
-    <div className="fly-information-container w-full h-screen flex flex-col justify-center items-center">
-        <div className="main-container w-4/6 border-1 border-sky-900 shadow-2xl py-2 px-6">
+    <div>
+        <Header/>
+    <div className="fly-information-container w-full mt-10 flex flex-col justify-center items-center">
+        <div className="main-container w-full min-w-max border-1 border-sky-900 shadow-2xl py-2 px-6">
         <MainHeader/>
         <MiniHeader/>
         <TicketInformation/>
         <PassengerInformation/>
         <InsuranceInformation/>
      </div>
-    <div className='button flex w-4/6 justify-end mt-2'>
+    <div className='button flex w-full min-w-[690px] justify-end mt-2'>
             <button type='button' className='bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-none'>Anasayfa</button>
      </div>
+</div>
 </div>
   )
 }
