5f243ffbb4a796ffa514e7f5a27ca689ce269cef
kadriozcan <kadri.ozcan@outlook.com>
2023-07-19 15:31:32 +0300

    Filter datas by Port selection

--- src/screens/FlyListScreen.js
+++ src/screens/FlyListScreen.js
@@ -2,31 +2,37 @@ import { useEffect, useState } from "react";
 import FlyList from "../components/FlyListScreen/FlyList";
 import axios from "axios";
 import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
-import { getDepartureLegs , getReturnLegs } from "../app/flyDataSlice";
+import { getDepartureLegs, getReturnLegs } from "../app/flyDataSlice";
 import { useDispatch } from "react-redux";
 
+export default function FlyListScreen(selectedOptions) {
+  const dispatch = useDispatch();
+  const departureLegs = useSelector((state) => state.data.departureLegs);
+  const returnLegs = useSelector((state) => state.data.returnLegs);
 
-export default function FlyListScreen() {
+  useEffect(() => {
+    dispatch(getDepartureLegs());
+  }, []);
 
-  const dispatch = useDispatch()
-  const departureLegs = useSelector((state) => state.data.departureLegs)
-  const returnLegs = useSelector((state) => state.data.returnLegs)
+  useEffect(() => {
+    dispatch(getReturnLegs());
+  }, []);
 
-  useEffect(()=>{
-    dispatch(getDepartureLegs())
-  },[])
-  
-  useEffect(()=>{
-    dispatch(getReturnLegs())
-  },[])
-
-
-  console.log(departureLegs);
-  console.log(returnLegs);
+  console.log("selectedOptions", selectedOptions);
+  let filteredDepartureLegs = departureLegs.filter(
+    (leg) =>
+      leg.depPort === selectedOptions.from.value &&
+      leg.arrPort === selectedOptions.to.value
+  );
+  let filteredReturnLegs = returnLegs.filter(
+    (leg) => leg.depPort === selectedOptions.from.value
+  );
+  console.log("filteredDepss:", filteredDepartureLegs);
+  console.log("filteredreturnss", filteredReturnLegs);
 
   return (
     <div className="px-96">
-      {departureLegs.map((leg, i) => {
+      {filteredDepartureLegs.map((leg, i) => {
         return (
           <FlyList
             key={i}
@@ -39,7 +45,7 @@ export default function FlyListScreen() {
           />
         );
       })}
-      {returnLegs.map((leg, i) => {
+      {filteredReturnLegs.map((leg, i) => {
         return (
           <FlyList
             key={i}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import FlyListScreen from './FlyListScreen';
-import axios from 'axios';
-import Select from 'react-select';
+import React, { useState, useEffect } from "react";
+import FlyListScreen from "./FlyListScreen";
+import axios from "axios";
+import Select from "react-select";
+import { useDispatch, useSelector } from "react-redux";
+import { getPorts } from "../app/flyDataSlice";
 
 const HomeScreen = () => {
-  const [ticketType, setTicketType] = useState('');
+  const [ticketType, setTicketType] = useState("");
   const [isSearch, setIsSearch] = useState(false);
-  const [PortList, setPortList] = useState([]);
   const [selectedOptionFrom, setSelectedOptionFrom] = useState();
   const [selectedOptionTo, setSelectedOptionTo] = useState();
 
+  const dispatch = useDispatch();
+  const portList = useSelector((state) => state.data.ports);
+
   useEffect(() => {
-    axios
-      .get('http://localhost:8001/Ports')
-      .then((res) => setPortList(res.data));
+    dispatch(getPorts());
   }, []);
 
-  const ports = PortList.map((p) => ({
+  const ports = portList.map((p) => ({
     label: p.explanation,
-    value: p.explanation,
+    value: p.code,
   }));
+  console.log(ports);
 
   function handleSelectFrom(data) {
     setSelectedOptionFrom(data);
@@ -36,88 +39,92 @@ const HomeScreen = () => {
     (p) => p.value !== selectedOptionTo?.value
   );
 
+  console.log("filteredfrom", filteredOptionsTo);
+
   const ticketTypeHandler = (e) => {
     setTicketType(e.target.value);
   };
 
   return (
-    <div className='px-[200px]'>
-      <h5 className=' text-center text-2xl pt-2 '> FLYBilet</h5>
-      <h1 className=' text-center text-4xl p-4'>
-        {' '}
-        Fly to the place of your dreams{' '}
+    <div className="px-[200px]">
+      <h5 className=" text-center text-2xl pt-2 "> FLYBilet</h5>
+      <h1 className=" text-center text-4xl p-4">
+        {" "}
+        Fly to the place of your dreams{" "}
       </h1>
-      <div className=' bg-[#d9d9d9] rounded'>
-        <h2 className='text-lg font-bold mb-4  text-center '>
+      <div className=" bg-[#d9d9d9] rounded">
+        <h2 className="text-lg font-bold mb-4  text-center ">
           Search for Flights
         </h2>
-        <form className='flex flex-row gap-4 max-w-auto mb-4 '>
-          <label htmlFor='trip'>Trip Type:</label>
+        <form className="flex flex-row gap-4 max-w-auto mb-4 ">
+          <label htmlFor="trip">Trip Type:</label>
           <select
-            id='trip'
-            name='trip'
+            id="trip"
+            name="trip"
             required
             onChange={ticketTypeHandler}
-            className='border border-gray-300 rounded-md p-2'
+            className="border border-gray-300 rounded-md p-2"
           >
-            <option value='one-way'>One-way</option>
-            <option value='round-trip'>Round Trip</option>
+            <option value="one-way">One-way</option>
+            <option value="round-trip">Round Trip</option>
           </select>
-          <label htmlFor='from'>From:</label>
+          <label htmlFor="from">From:</label>
           <Select
             options={filteredOptionsFrom}
-            placeholder='select your destination'
+            placeholder="select your destination"
             value={selectedOptionFrom}
             onChange={handleSelectFrom}
             isSearchable={true}
           />
 
-          <label htmlFor='to'>To:</label>
+          <label htmlFor="to">To:</label>
           <Select
             options={filteredOptionsTo}
-            placeholder='select your destination'
+            placeholder="select your destination"
             value={selectedOptionTo}
             onChange={handleSelectTo}
             isSearchable={true}
-            className='min-w-[150px]'
+            className="min-w-[150px]"
           />
 
-          <label htmlFor='date'>Date:</label>
-          <input type='date' id='date' name='date' required className='input' />
+          <label htmlFor="date">Date:</label>
+          <input type="date" id="date" name="date" required className="input" />
 
-          {ticketType === 'round-trip' && (
+          {ticketType === "round-trip" && (
             <>
-              <label htmlFor='returnDate'>Return Date:</label>
+              <label htmlFor="returnDate">Return Date:</label>
               <input
-                type='date'
-                id='returnDate'
-                name='returnDate'
+                type="date"
+                id="returnDate"
+                name="returnDate"
                 required
-                className='border border-gray-300 rounded-md p-2'
+                className="border border-gray-300 rounded-md p-2"
               />
             </>
           )}
 
-          <label htmlFor='passengers'>Number of Passengers:</label>
+          <label htmlFor="passengers">Number of Passengers:</label>
           <input
-            type='number'
-            id='passengers'
-            name='passengers'
-            min='1'
+            type="number"
+            id="passengers"
+            name="passengers"
+            min="1"
             required
-            className='input'
+            className="input"
           />
 
           <button
-            type='button'
-            className='bg-gray-900 text-white p-2 rounded-md'
+            type="button"
+            className="bg-gray-900 text-white p-2 rounded-md"
             onClick={() => setIsSearch(true)}
           >
             Search
           </button>
         </form>
-        {isSearch && <FlyListScreen />}
-        <footer className='bg-gray-100 p-2 text-center'>
+        {isSearch && (
+          <FlyListScreen from={selectedOptionFrom} to={selectedOptionTo} />
+        )}
+        <footer className="bg-gray-100 p-2 text-center">
           <p>&copy; 2023 flyBilet Website. All rights reserved.</p>
         </footer>
       </div>
