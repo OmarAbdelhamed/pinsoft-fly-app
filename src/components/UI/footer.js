36fb0ca95fb85e8c9431b19644fe4ac00359240a
Omar Abdelhamed <omar792001@gmail.com>
2023-07-23 01:22:28 +0300

    .

--- src/components/UI/footer.js
+++ src/components/UI/footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'remixicon/fonts/remixicon.css';
+import { GiLibertyWing } from 'react-icons/gi';
 
 const Footer = () => {
   return (
     <footer className='bg-gradient-to-t from-black to-[#1a0f39] shadow-inner '>
       <div className='grid grid-cols-2 md:grid-cols-3 gap-20 max-w-[1200px] m-auto px-20 py-4'>
         <div className='text-extra-light'>
-          <h3 className='text-xl font-semibold mb-3'>Flivan</h3>
+          <h3 className='text-xl font-semibold mb-3 flex items-center gap-1'><GiLibertyWing  className=''/>FLYBilet</h3>
           <p>
             Where Excellence Takes Flight. With a strong commitment to customer
             satisfaction and a passion for air travel.
