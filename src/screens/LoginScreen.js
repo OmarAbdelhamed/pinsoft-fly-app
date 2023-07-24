4afc099653cc151c133c4a38e35026bc592ef306
Ibrahim Gelmez <ibrg47@gmail.com>
2023-07-24 15:03:04 +0300

    some bugs fixed

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -9,7 +9,6 @@ export default function LoginScreen() {
   function passwordHandler(e) {
     setPassword(e.target.value);
   }
-  console.log(email)
 
   return (
     <div className="flex flex-col mt-36 justify-center items-center">
@@ -18,11 +17,13 @@ export default function LoginScreen() {
           className="border-2 mb-3  py-2 px-2 rounded-lg"
           placeholder="Email"
           onChange={emailHandler}
+          value={email}
         />
         <input
           className="border-2 mt-3 py-2 px-2 rounded-lg"
           placeholder="Password"
           onChange={passwordHandler}
+          value={password}
         />
       </div>
       <button className="bg-blue-400 text-white font-bold rounded-xl px-9 py-2 mt-9 focus:outline-none focus:ring-2">
