3bf73177d89371db293100219c02da99e2d85534
kadriozcan <kadri.ozcan@outlook.com>
2023-07-25 14:29:51 +0300

    round trip selection

--- src/components/FlyListScreen/FlyList.js
+++ src/components/FlyListScreen/FlyList.js
@@ -1,5 +1,5 @@
+import { useState } from "react";
 import { SiChinasouthernairlines } from "react-icons/si";
-import { useNavigate } from "react-router";
 export default function FlyList({
   flightNo,
   depTime,
@@ -9,15 +9,14 @@ export default function FlyList({
   airline,
   depPort,
   arrPort,
+  type,
+  leg,
 }) {
   const [depTimeSaat, depTimeDakika] = depTime.split(":");
   const [arrTimeSaat, arrTimeDakika] = arrTime.split(":");
 
-  const navigate = useNavigate();
-
-  const nextPageHandler = () => {
-    navigate("/Details");
-  };
+  const [selectedDepFlight, setSelectedDepFlight] = useState();
+  const [selectedRetFlight, setSelectedRetFlight] = useState();
 
   const depTarih = new Date();
   depTarih.setHours(depTimeSaat, depTimeDakika, 0);
@@ -32,9 +31,16 @@ export default function FlyList({
   const sure = `${farkSaat} sa ${farkDakika} dk`;
   console.log(farkMilisaniye);
 
+  const handleSelection = () => {
+    if (type === "dep") {
+      setSelectedDepFlight(leg);
+    } else {
+      setSelectedRetFlight(leg);
+    }
+  };
   return (
     <div
-      onClick={nextPageHandler}
+      onClick={handleSelection}
       className=" cursor-pointer bg-white w-[350px] md:w-auto mb-2 border-2 border-slate-300  lg:min-h-[140px]  lg:p-4 rounded-lg md:hover:scale-105  lg:m-4 grid grid-cols-3 items-center place-content-center text-center backdrop-blur-sm bg-white/90 "
     >
       <div className="text-2xl flex flex-row justify-center items-center gap-3">
--- src/screens/FlyListScreen.js
+++ src/screens/FlyListScreen.js
@@ -1,30 +1,16 @@
-import FlyList from '../components/FlyListScreen/FlyList';
-
+import FlyList from "../components/FlyListScreen/FlyList";
+import { useNavigate } from "react-router";
 export default function FlyListScreen(props) {
-  return (
-    <div className='lg:px-36 lg:m-4  pt-5'>
-      {props.filteredDepartureLegs.map((leg, i) => {
-        return (
-          <FlyList
-            key={i}
-            airline={leg.airline}
-            flightNo={leg.flightNo}
-            depTime={leg.depTime}
-            arrTime={leg.arrTime}
-            price={leg.priceDetail.basePrice.amount}
-            currency={leg.priceDetail.basePrice.currency}
-            date={leg.flightDate}
-            depPort={leg.depPort}
-            arrPort={leg.arrPort}
-          />
-        );
-      })}
-      <br />
-      <h2 className='text-center text-4xl  mb-[50px] '>Return Flights</h2>
+  const navigate = useNavigate();
 
-      {props.filteredReturnLegs.map((leg, i) => {
-        return (
-          <>
+  const nextPageHandler = () => {
+    navigate("/Details");
+  };
+  return (
+    <>
+      <div className="lg:px-36 lg:m-4  pt-5">
+        {props.filteredDepartureLegs.map((leg, i) => {
+          return (
             <FlyList
               key={i}
               airline={leg.airline}
@@ -36,10 +22,36 @@ export default function FlyListScreen(props) {
               date={leg.flightDate}
               depPort={leg.depPort}
               arrPort={leg.arrPort}
+              type="dep"
+              leg={leg}
             />
-          </>
-        );
-      })}
-    </div>
+          );
+        })}
+        <br />
+        <h2 className="text-center text-4xl  mb-[50px] ">Return Flights</h2>
+
+        {props.filteredReturnLegs.map((leg, i) => {
+          return (
+            <>
+              <FlyList
+                key={i}
+                airline={leg.airline}
+                flightNo={leg.flightNo}
+                depTime={leg.depTime}
+                arrTime={leg.arrTime}
+                price={leg.priceDetail.basePrice.amount}
+                currency={leg.priceDetail.basePrice.currency}
+                date={leg.flightDate}
+                depPort={leg.depPort}
+                arrPort={leg.arrPort}
+                type="ret"
+                leg={leg}
+              />
+            </>
+          );
+        })}
+      </div>
+      <button onClick={nextPageHandler}>Continue</button>
+    </>
   );
 }
