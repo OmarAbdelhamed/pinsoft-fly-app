119533daf7340ba758f77c6b258b6e7d9f0e9bae
Omar Abdelhamed <omar792001@gmail.com>
2023-07-25 10:22:19 +0300

    ..

--- src/components/FlyingDetailScreen/ContactDetail.js
+++ src/components/FlyingDetailScreen/ContactDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactDetail = () => {
   return (
-    <div className='p-4 rounded-lg'>
+    <div className='p-14 rounded-lg'>
       <p className='text-2xl font-bold mb-4 text-center'>Contact Information</p>
       <div className='flex flex-col justify-center gap-3 items-center mb-4'>
         <div className='App flex justify-center items-center '>
@@ -12,7 +12,7 @@ const ContactDetail = () => {
               placeholder='Email'
               className='  w-[255px] h-[44px] text-[20px] p-2 text-slate-600 border-slate-400 border-2 rounded-lg border-opacity-50 outline-none focus:border-primary-color-dark placeholder-gray-300 placeholder-opacity-0 transition duration-200'
             />
-            <span className='text-2xl cursor-text  text-black text-opacity-50 bg-white absolute left-2 top-1 px-1 transition duration-200 input-text'>
+            <span className='text-2xl cursor-text  text-black text-opacity-50 bg-transparent absolute left-2 top-1 px-1 transition duration-200 input-text'>
               Email
             </span>
           </label>
@@ -24,7 +24,7 @@ const ContactDetail = () => {
               placeholder='Phone Number'
               className='  w-[255px] h-[44px] text-[20px] p-2 text-slate-600 border-slate-400 border-2 rounded-lg border-opacity-50 outline-none focus:border-primary-color-dark placeholder-gray-300 placeholder-opacity-0 transition duration-200'
             />
-            <span className='text-2xl cursor-text  text-black text-opacity-50 bg-white absolute left-2 top-1 px-1 transition duration-200 input-text'>
+            <span className='text-2xl cursor-text  text-black text-opacity-50 bg-transparent absolute left-2 top-1 px-1 transition duration-200 input-text'>
               Phone Number
             </span>
           </label>
