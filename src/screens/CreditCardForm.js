aefe6ac291d895f272599cdfd440febb124c72cb
Omar Abdelhamed <omar792001@gmail.com>
2023-07-25 15:40:42 +0300

    payment styling

--- src/screens/CreditCardForm.js
+++ src/screens/CreditCardForm.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import Cards from "react-credit-cards-2";
-import "react-credit-cards-2/dist/es/styles-compiled.css";
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards-2';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
 
 const CreditCardForm = () => {
   const [state, setState] = useState({
-    number: "",
-    expiry: "",
-    cvc: "",
-    name: "",
-    focus: "",
+    number: '',
+    expiry: '',
+    cvc: '',
+    name: '',
+    focus: '',
   });
 
   const handleInputChange = (evt) => {
@@ -22,7 +22,7 @@ const CreditCardForm = () => {
   };
 
   return (
-    <div>
+    <div className='-mt-2'>
       <Cards
         number={state.number}
         expiry={state.expiry}
@@ -30,50 +30,50 @@ const CreditCardForm = () => {
         name={state.name}
         focused={state.focus}
       />
-      <form>
-        <div className="form-group mb-3 mt-4">
+      <form className='flex flex-col items-center justify-center '>
+        <div className='  mb-3 mt-4 '>
           <input
-            type="tel"
-            name="number"
-            className="form-control"
-            placeholder="Card Number"
-            pattern="[\d| ]{16,22}"
+            type='tel'
+            name='number'
+            className=' w-[255px]  border focus:border-none rounded-lg h-[44px] p-2'
+            placeholder='Card Number'
+            pattern='[\d| ]{16,22}'
             required
             onChange={handleInputChange}
             onFocus={handleInputFocus}
           />
         </div>
-        <div className="form-group mb-3">
+        <div className=' mb-3 '>
           <input
-            type="text"
-            name="name"
-            className="form-control"
-            placeholder="Name"
+            type='text'
+            name='name'
+            className=' w-[255px] border rounded-lg h-[44px] p-2'
+            placeholder='Name'
             required
             onChange={handleInputChange}
             onFocus={handleInputFocus}
           />
         </div>
-        <div className="row mb-3">
-          <div className="col-6">
+        <div className='mb-3 '>
+          <div className='col-6'>
             <input
-              type="tel"
-              name="expiry"
-              className="form-control"
-              placeholder="Valid Thru"
-              pattern="\d\d/\d\d"
+              type='tel'
+              name='expiry'
+              className=' w-[255px] border rounded-lg h-[44px] p-2'
+              placeholder='Valid Thru'
+              pattern='\d\d/\d\d'
               required
               onChange={handleInputChange}
               onFocus={handleInputFocus}
             />
           </div>
-          <div className="col-6">
+          <div className='col-6 mb-1 mt-3 '>
             <input
-              type="tel"
-              name="cvc"
-              className="form-control"
-              placeholder="CVC"
-              pattern="\d{3,4}"
+              type='tel'
+              name='cvc'
+              className='form-control text-lg border rounded-lg w-[50px] h-[35px] p-2'
+              placeholder='CVC'
+              pattern='\d{3,4}'
               required
               onChange={handleInputChange}
               onFocus={handleInputFocus}
