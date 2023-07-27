3cac7954db032eb117423c974606aa568b93a2a2
Omar Abdelhamed <omar792001@gmail.com>
2023-07-27 17:17:45 +0300

    return Flight Price details added

--- src/components/FlyingDetailScreen/PriceDetail.js
+++ src/components/FlyingDetailScreen/PriceDetail.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const PriceDetail = () => {
-
   const selectedFlight = useSelector((state) => state.data.selectedDepFlight);
-  const selectedRetFlight = useSelector((state) => state.data.selectedRetFlight);
+  const selectedRetFlight = useSelector(
+    (state) => state.data.selectedRetFlight
+  );
   return (
     <>
       <div className='border p-10 relative shadow-xl bg-white rounded-lg h-auto max-w-[1200px] flex flex-col justify-center items-center'>
@@ -13,6 +14,9 @@ const PriceDetail = () => {
             Price Details
           </h2>
           <div>
+            <h2 className='text-xl font-semibold mb-4 text-center font-Headlines'>
+              One Way Ticket
+            </h2>
             <table className=' text-left w-[600px] '>
               <thead>
                 <tr className='border-b-2 border-gray-300'>
@@ -27,22 +31,97 @@ const PriceDetail = () => {
                 <tr className='border-b border-gray-300'>
                   <td className='px-4 py-2'>1 Adults</td>
                   <td className='px-4 py-2'>
-                    {selectedFlight.passengerPrices[0].priceDetail.basePrice.amount}<span>₺</span>
+                    {
+                      selectedFlight.passengerPrices[0].priceDetail.basePrice
+                        .amount
+                    }
+                    <span>₺</span>
                   </td>
                   <td className='px-4 py-2'>
-                  {selectedFlight.passengerPrices[0].priceDetail.totalTax.amount}<span>₺</span>
+                    {
+                      selectedFlight.passengerPrices[0].priceDetail.totalTax
+                        .amount
+                    }
+                    <span>₺</span>
                   </td>
                   <td className='px-4 py-2'>
-                  {selectedFlight.passengerPrices[0].priceDetail.surcharge.amount}<span>₺</span>
+                    {
+                      selectedFlight.passengerPrices[0].priceDetail.surcharge
+                        .amount
+                    }
+                    <span>₺</span>
                   </td>
                   <td className='px-4 py-2'>
-                  {selectedFlight.passengerPrices[0].priceDetail.salesPrice.amount}<span>₺</span>
+                    {
+                      selectedFlight.passengerPrices[0].priceDetail.salesPrice
+                        .amount
+                    }
+                    <span>₺</span>
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
+        {(Object.values(selectedRetFlight).length !== 0 && (
+          <>
+            <div className='flex flex-col mt-[10px]'>
+              <h2 className='text-xl font-semibold mb-4 text-center font-Headlines'>
+                Return Ticket
+              </h2>
+              <div>
+                <table className=' text-left w-[600px] '>
+                  <thead>
+                    <tr className='border-b-2 border-gray-300'>
+                      <th className='px-4 py-2'>Passenger Type</th>
+                      <th className='px-4 py-2'>Price</th>
+                      <th className='px-4 py-2'>Taxes</th>
+                      <th className='px-4 py-2'>surcharge</th>
+                      <th className='px-4 py-2'>Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className='border-b border-gray-300'>
+                      <td className='px-4 py-2'>1 Adults</td>
+                      <td className='px-4 py-2'>
+                        {
+                          selectedRetFlight.passengerPrices[0].priceDetail
+                            .basePrice.amount
+                        }
+                        <span>₺</span>
+                      </td>
+                      <td className='px-4 py-2'>
+                        {
+                          selectedRetFlight.passengerPrices[0].priceDetail
+                            .totalTax.amount
+                        }
+                        <span>₺</span>
+                      </td>
+                      <td className='px-4 py-2'>
+                        {
+                          selectedRetFlight.passengerPrices[0].priceDetail
+                            .surcharge.amount
+                        }
+                        <span>₺</span>
+                      </td>
+                      <td className='px-4 py-2'>
+                        {
+                          selectedRetFlight.passengerPrices[0].priceDetail
+                            .salesPrice.amount
+                        }
+                        <span>₺</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </>
+        )) || (
+          <h1 className='text-xl font-bold mt-10  text-center text-slate-800 font-Headlines'>
+            Your did not select Round trip
+          </h1>
+        )}
       </div>
     </>
   );
